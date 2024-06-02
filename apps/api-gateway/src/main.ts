12f72edb49efa023f05e430ff588f8cd98e8804d
Jason Pan <jason.pan@team.telstra.com>
2024-06-02 22:28:26 +1000

    Add Swagger docs
    Signed-off-by: Jason Pan <jason.pan@team.telstra.com>

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ApiGatewayModule } from './api-gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('SWAPI Service')
+    .setDescription('The SWAPI Service API description')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- apps/query-manager/src/main.ts
+++ apps/query-manager/src/main.ts
@@ -3,7 +3,6 @@ import { NatsOptions, Transport } from '@nestjs/microservices';
 import { QueryManagerModule } from './query-manager.module';
 
 async function bootstrap() {
-  // console.log(`test123`, process.env.NATS_URI);
   const app = await NestFactory.createMicroservice<NatsOptions>(QueryManagerModule, {
     transport: Transport.NATS,
     options: {
@@ -11,15 +10,5 @@ async function bootstrap() {
     },
   });
   await app.listen();
-  // const app = await NestFactory.create(QueryManagerModule);
-  // app.connectMicroservice<NatsOptions>({
-  //   transport: Transport.NATS,
-  //   options: {
-  //     servers: [process.env.NATS_URI!],
-  //   },
-  // });
-
-  // await app.startAllMicroservices();
-  // await app.listen(process.env.PORT!);
 }
 bootstrap();
